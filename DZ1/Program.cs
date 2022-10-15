d667a7459192dcb67f3ad0626c7f524e3bb5bdfa
Maxim Sorokin <maximka773@mail.ru>
2022-10-15 14:04:13 +0300

    add number 2

---
+++ DZ1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер задачи: 2, 4, 6 или 8");
+int Z = Convert.ToInt32(Console.ReadLine());
+
+// Задача №2
+if (Z == 2)
+{
+    Console.WriteLine("Задача 2. Введите два числа");
+    int A2 = Convert.ToInt32(Console.ReadLine());
+    int B2 = Convert.ToInt32(Console.ReadLine());
+
+    if (A2 > B2)
+    {
+        Console.WriteLine($"Число {A2} больше, чем число {B2}");
+    }
+    else
+    {
+        if (A2 == B2)
+        {
+            Console.WriteLine("Числа равны");
+        }
+        else
+        {
+            Console.WriteLine($"Число {B2} больше, чем число {A2}");
+        }
+    }
+}
