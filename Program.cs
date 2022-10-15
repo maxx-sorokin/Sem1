9338ba17ce5e7f8ae2d71d3796c7615a39a40c59
Maxim Sorokin <maximka773@mail.ru>
2022-10-15 11:35:44 +0300

    5 задача

--- Program.cs
+++ Program.cs
@@ -1,32 +1,14 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число от 1 до 7");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1)
+int i = -N;
+
+while (i < N)
 {
-    Console.WriteLine("Понедельник");
+    Console.Write(i);
+    Console.Write(", ");
+    i += 1;
 }
-if (number == 2)
-{
-    Console.WriteLine("Вторник");
-}
-if (number == 3)
-{
-    Console.WriteLine("Среда");
-}
-if (number == 4)
-{
-    Console.WriteLine("Четверг");
-}
-if (number == 5)
-{
-    Console.WriteLine("Пятница");
-}
-if (number == 6)
-{
-    Console.WriteLine("Суббота");
-}
-if (number == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
+
+Console.Write(i);
