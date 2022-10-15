93f37d98c1f9c9c345c8098f6e34e01fdbd504a1
Maxim Sorokin <maximka773@mail.ru>
2022-10-15 11:48:02 +0300

    5 задача исправленная

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите трёхзначаное число");
+Console.WriteLine("Введите число");
 int N = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(N % 10);
+int i = -N;
+
+while (i < N)
+{
+    Console.Write($"{i}, ");
+    i++;
+}
+
+Console.Write(i);
